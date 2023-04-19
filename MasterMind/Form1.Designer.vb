54cdecbf454f3771591bc8fede294401a9750a7e
Nicolas Tassin <nicolas.tassin23@gmail.com>
2023-04-19 23:08:12 +0200

    Ajoutez des fichiers projet.

---
+++ MasterMind/Form1.Designer.vb
@@ -0,0 +1,64 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form remplace la méthode Dispose pour nettoyer la liste des composants.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requise par le Concepteur Windows Form
+    Private components As System.ComponentModel.IContainer
+
+    'REMARQUE : la procédure suivante est requise par le Concepteur Windows Form
+    'Elle peut être modifiée à l'aide du Concepteur Windows Form.  
+    'Ne la modifiez pas à l'aide de l'éditeur de code.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.ComboPseudo = New System.Windows.Forms.ComboBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'ComboPseudo
+        '
+        Me.ComboPseudo.FormattingEnabled = True
+        Me.ComboPseudo.Location = New System.Drawing.Point(12, 12)
+        Me.ComboPseudo.Name = "ComboPseudo"
+        Me.ComboPseudo.Size = New System.Drawing.Size(174, 24)
+        Me.ComboPseudo.TabIndex = 0
+        Me.ComboPseudo.UseWaitCursor = True
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(317, 64)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(181, 184)
+        Me.Button1.TabIndex = 1
+        Me.Button1.Text = "VALIDER"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(510, 320)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.ComboPseudo)
+        Me.Name = "Form1"
+        Me.RightToLeftLayout = True
+        Me.Text = "Entrez votre pseudo"
+        Me.UseWaitCursor = True
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents ComboPseudo As ComboBox
+    Friend WithEvents Button1 As Button
+End Class
---
+++ MasterMind/Form1.vb
@@ -0,0 +1,26 @@
+﻿Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+
+    Private Sub ComboPseudo_SelectedIndexChanged(ByVal sender As Object, ByVal e As System.ComponentModel.CancelEventArgs) Handles ComboPseudo.Validating
+        Dim pseudo As String = ComboPseudo.Text
+        If ComboPseudo.Items.Contains(pseudo, StringComparer.OrdinalIgnoreCase) = True Then
+            MsgBox("Ce pseudo est déjà utilisé")
+            e.Cancel = True
+        End If
+
+    End Sub
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim pseudo As String = ComboPseudo.Text.ToLower()
+        If ComboPseudo.Items.Contains(pseudo) Then
+            MsgBox("Ce pseudo est déjà utilisé")
+        Else
+            ComboPseudo.Items.Add(pseudo)
+            MsgBox("Pseudo " & pseudo & " ajouté")
+        End If
+    End Sub
+
+
+End Class
